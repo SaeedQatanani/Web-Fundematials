bd0e0b5e4de3daf6e81b43dcdaa1fa636aa87d4f
SaeedQatanani <saeedqatananil@gmail.com>
2022-08-30 11:47:51 +0300

    submission

--- JS/Buttom Clicker/scripte.js
+++ JS/Buttom Clicker/scripte.js
@@ -5,3 +5,11 @@ function changeText(element) {
 function removeText(element) {
     element.remove();
 }
+
+function addShadow(element) {
+    element.classList.add("shadow");
+}
+
+function removeShadow(element) {
+    element.classList.remove("shadow");
+}
