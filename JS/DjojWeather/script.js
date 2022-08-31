af672174dc4ce0f8a41ea1a5e90d1453addda4f3
SaeedQatanani <saeedqatananil@gmail.com>
2022-08-31 17:21:44 +0300

    submission

---
+++ JS/DjojWeather/script.js
@@ -0,0 +1,20 @@
+var messageDismisser = document.querySelector("footer");
+var temp = document.querySelectorAll("span");
+var selectValue = document.querySelector("#temp").value;
+
+function dismiss() {
+    messageDismisser.remove();
+}
+
+function changeTemp(selectValue) {
+    if (selectValue == "feh") {
+        for (var i = 0; i < temp.length; i++) {
+            temp[i].innerText =Math.round((temp[i].innerText * 1.8) + 32);
+        }
+    }
+    else if (selectValue == "cel") {
+        for (var i = 0; i < temp.length; i++) {
+            temp[i].innerText = Math.round((temp[i].innerText - 32) * (5/9));
+        }
+    }
+}
